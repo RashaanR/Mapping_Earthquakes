109fcf1cfcb8ba568dde6de806ad9791c79fece6
unknown <rashaan.rasheed@gmail.com>
2022-08-24 02:35:30 -0700

    practice

--- Past 7 days/static/js/logic.js
+++ Past 7 days/static/js/logic.js
@@ -3,14 +3,15 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [
-      43.7, -80
-    ],
-    zoom: 10,
-  });
+  center: [
+    39.5, -98.5
+  ],
+  zoom: 3,
+});
+
 
  // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -28,18 +29,106 @@ let baseMaps = {
   "Satellite Streets": Satellite
 };    
 
+let earthquakes = new L.LayerGroup();
+let overlays = { 
+  "Earthquakes": earthquakes
+};
+
+
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 
-let torontoData = "https://raw.githubusercontent.com/RashaanR/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
+let EQD = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// Retrieve the earthquake GeoJSON data.
+d3.json(EQD).then(function(data) 
+{
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, 
+    {
+    
+    pointToLayer: function(feature, latlng) 
+        {
+          console.log(data);
+          return L.circleMarker(latlng);
+        },
+          // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);}
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
+        // Create a legend control object.
+        let legend = L.control({
+        position: "bottomright"});
+        // Create a legend control object.
+        legend.onAdd = function() 
+        {
+          let div = L.DomUtil.create("div", "info legend");
+          const magnitudes = [0, 1, 2, 3, 4, 5];
+          const colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"];
+            for (var i = 0; i < magnitudes.length; i++) {
+              console.log(colors[i]);
+              div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i> " +
+                magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");}
+            return div;
+        };
+         
+          legend.addTo(map);
+});
+
+
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data);
-//  Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
-});
+// Creating a GeoJSON layer with the retrieved data.
+// Creating a GeoJSON layer with the retrieved data.
+// Creating a GeoJSON layer with the retrieved data.
 
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
